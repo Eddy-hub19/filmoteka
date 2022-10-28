564757590285200aef1e7b3e3de3f5282df03198
Sergey <104456968+Solod-S@users.noreply.github.com>
2022-10-28 23:33:59 +0300

    hover + search visual

--- src/js/moviesListRenderByTopAndSearch.js
+++ src/js/moviesListRenderByTopAndSearch.js
@@ -1,91 +1,105 @@
-import TmDbApi from "./services/fetchApi"
-import genres from "./services/genres"
-import { modalListener } from "./modal"
+import TmDbApi from "./services/fetchApi";
+import genres from "./services/genres";
+import { modalListener } from "./modal";
 
-const Api = new TmDbApi()
+const Api = new TmDbApi();
 export const defaultPoster =
-    "https://tn.fishki.net/26/upload/post/2018/04/20/2577020/afrikanskie-postery-k-gollivudskim-blokbasteram-6.jpg"
+  "https://tn.fishki.net/26/upload/post/2018/04/20/2577020/afrikanskie-postery-k-gollivudskim-blokbasteram-6.jpg";
 
 export const moviesListRenderByTopAndSearch = {
-    movieList: document.querySelector(".gallery"),
-    paginationControlBtns: document.querySelectorAll(".js_pagination--btn"),
-    searchForm: document.querySelector(".js-search__form"),
-    options: {
-        query: "",
-        page: 1,
-        totalPages: null,
-        // per_page: 15,
-    },
-    onSearchForm(event) {
-        event.preventDefault()
-        const { options } = this
-        const currentQuery = this.options.query
-        const form = event.currentTarget
-        const searchQuery = form.elements.searchQuery.value
-        if (searchQuery === currentQuery) {
-            form.reset()
-            return
-        }
-        options.query = searchQuery
-        options.page = 1
-        form.reset()
+  movieList: document.querySelector(".gallery"),
+  paginationControlBtns: document.querySelectorAll(".js_pagination--btn"),
+  searchForm: document.querySelector(".js-search__form"),
+  searchWarning: document.querySelector(".search__warning"),
+  options: {
+    query: "",
+    page: 1,
+    totalPages: null,
+    // per_page: 15,
+  },
+  onSearchForm(event) {
+    event.preventDefault();
+    const { options } = this;
+    const currentQuery = this.options.query;
+    const form = event.currentTarget;
+    const searchQuery = form.elements.searchQuery.value;
+    if (searchQuery === currentQuery || searchQuery === "") {
+      form.reset();
+      return;
+    }
+    options.query = searchQuery;
+    options.page = 1;
+    form.reset();
 
-        this.render()
-    },
-    changePage(event) {
-        const action = event.currentTarget.dataset.page
-        const { options } = this
-        if (action === "prev" && this.options.page > 1) {
-            options.page -= 1
+    this.render();
+  },
+  changePage(event) {
+    const action = event.currentTarget.dataset.page;
+    const { options } = this;
+    if (action === "prev" && this.options.page > 1) {
+      options.page -= 1;
 
-            this.render()
-        } else if (action === "next" && this.options.page < this.options.totalPages) {
-            options.page += 1
+      this.render();
+    } else if (
+      action === "next" &&
+      this.options.page < this.options.totalPages
+    ) {
+      options.page += 1;
 
-            this.render()
-        }
-    },
-    async render() {
-        const { options } = this
-        const { page, query } = this.options
+      this.render();
+    }
+  },
+  async render() {
+    const { options } = this;
+    const { page, query } = this.options;
 
-        if (query) {
-            try {
-                const filmResponse = await Api.fetchSearchMovies(query, page)
-                options.totalPages = filmResponse.total_pages
-                const films = filmResponse.results
-                this.createMarkUp(this.preparingForMarkUp(films))
-            } catch (error) {
-                console.log(error, `Попробуйте перезагрузить страницу`)
-            }
+    if (query) {
+      try {
+        const filmResponse = await Api.fetchSearchMovies(query, page);
+        options.totalPages = filmResponse.total_pages;
+        const films = filmResponse.results;
+        if (films.length === 0) {
+          this.searchWarning.classList.remove("hidden");
         } else {
-            try {
-                const filmResponse = await Api.fetchTrendingMovies(page)
-                options.totalPages = filmResponse.total_pages
-
-                const films = filmResponse.results
-                this.createMarkUp(this.preparingForMarkUp(films))
-            } catch (error) {
-                console.log(error, `Попробуйте перезагрузить страницу`)
-            }
+          this.searchWarning.classList.add("hidden");
         }
-    },
-    preparingForMarkUp(films) {
-        return films.map(({ id, title, poster_path, vote_average, release_date, genre_ids }) => ({
-            id,
-            title,
-            poster_path: poster_path ? "https://image.tmdb.org/t/p/w500" + poster_path : defaultPoster,
-            vote_average,
-            release_date: release_date ? release_date.split("-") : "`2000",
-            genre_ids: this.calculatingGenres(genre_ids),
-        }))
-    },
+        this.createMarkUp(this.preparingForMarkUp(films));
+      } catch (error) {
+        console.log(error, `Попробуйте перезагрузить страницу`);
+      }
+    } else {
+      try {
+        const filmResponse = await Api.fetchTrendingMovies(page);
+        options.totalPages = filmResponse.total_pages;
 
-    createMarkUp(preparedMovies) {
-        const { movieList } = this
-        const moviesMarkUp = preparedMovies
-            .map(({ id, title, poster_path, vote_average, release_date, genre_ids }) => {
-                return `<li class="gallery__card" data-id=${id}>
+        const films = filmResponse.results;
+        this.createMarkUp(this.preparingForMarkUp(films));
+      } catch (error) {
+        console.log(error, `Попробуйте перезагрузить страницу`);
+      }
+    }
+  },
+  preparingForMarkUp(films) {
+    return films.map(
+      ({ id, title, poster_path, vote_average, release_date, genre_ids }) => ({
+        id,
+        title,
+        poster_path: poster_path
+          ? "https://image.tmdb.org/t/p/w500" + poster_path
+          : defaultPoster,
+        vote_average,
+        release_date: release_date ? release_date.split("-") : "`2000",
+        genre_ids: this.calculatingGenres(genre_ids),
+      })
+    );
+  },
+
+  createMarkUp(preparedMovies) {
+    const { movieList } = this;
+    const moviesMarkUp = preparedMovies
+      .map(
+        ({ id, title, poster_path, vote_average, release_date, genre_ids }) => {
+          return `<li class="gallery__card" data-id=${id}>
           <img
             src=${poster_path}
             data-source=${poster_path}
@@ -96,44 +110,55 @@ export const moviesListRenderByTopAndSearch = {
           <div class="gallery__data">
             <div class="gallery__name">Servant of the People</div>
             <div class="gallery__stats">
-              <p class="gallery__details">${genre_ids.join(", ")} | ${release_date[0]}</p>
+              <p class="gallery__details">${genre_ids.join(", ")} | ${
+            release_date[0]
+          }</p>
          
               <p class="gallery__rating">${vote_average.toFixed(1)}</p>
             </div>
           </div>
         </li>
-    `
-            })
-            .join("")
-        movieList.innerHTML = ""
-        movieList.insertAdjacentHTML("beforeend", moviesMarkUp)
-        modalListener()
-    },
-    calculatingGenres(genre_ids) {
-        const sortGenres = genres
-            .filter((genre) => {
-                for (const id of genre_ids) {
-                    if (id === genre.id) {
-                        return genre
-                    }
-                }
-            })
-            .map((genre) => genre.name)
-        if (sortGenres.length > 2) {
-            return [...sortGenres.slice(0, 2), "Other"]
-        } else {
-            return sortGenres
+    `;
         }
-    },
-}
+      )
+      .join("");
+    movieList.innerHTML = "";
+    movieList.insertAdjacentHTML("beforeend", moviesMarkUp);
+    modalListener();
+  },
+  calculatingGenres(genre_ids) {
+    const sortGenres = genres
+      .filter((genre) => {
+        for (const id of genre_ids) {
+          if (id === genre.id) {
+            return genre;
+          }
+        }
+      })
+      .map((genre) => genre.name);
+    if (sortGenres.length > 2) {
+      return [...sortGenres.slice(0, 2), "Other"];
+    } else {
+      return sortGenres;
+    }
+  },
+};
 
 // moviesListRenderByTopAndSearch.render();
 
 moviesListRenderByTopAndSearch.paginationControlBtns.forEach((el) =>
-    el.addEventListener("click", moviesListRenderByTopAndSearch.changePage.bind(moviesListRenderByTopAndSearch))
-)
+  el.addEventListener(
+    "click",
+    moviesListRenderByTopAndSearch.changePage.bind(
+      moviesListRenderByTopAndSearch
+    )
+  )
+);
 
 moviesListRenderByTopAndSearch.searchForm.addEventListener(
-    "submit",
-    moviesListRenderByTopAndSearch.onSearchForm.bind(moviesListRenderByTopAndSearch)
-)
+  "submit",
+  moviesListRenderByTopAndSearch.onSearchForm.bind(
+    moviesListRenderByTopAndSearch
+  )
+);
+console.log(moviesListRenderByTopAndSearch.searchWarning);
