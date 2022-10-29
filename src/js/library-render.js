24290aea848b3b25b04bc99fa464327052f5848c
OlenaBielova <belova.elena11@gmail.com>
2022-10-29 16:47:33 +0300

    add modal listener, refactored code

--- src/js/library-render.js
+++ src/js/library-render.js
@@ -1,56 +1,50 @@
 import TmDbApi from "./services/fetchApi";
 import allGenres from "./services/genres";
+import { modalListener } from "./modal";
 const Api = new TmDbApi();
-
 
 export const library = {
     movieList: document.querySelector(".gallery"),
     options: {
-        page:1,
+    page:1,
     },
     
 watchedRender() {
     const watchedMoviesID = JSON.parse(localStorage.getItem("storage")).watched;
-    console.log(watchedMoviesID);
-    const films = [];
-        watchedMoviesID.map(async (movieId) => {
-        this.options.id = movieId;
-        console.log(this.options.id);
+    const movies = [];
+
+    watchedMoviesID.map(async (movieId) => {   
         try {
-            const film = await Api.fetchMovieDetail(movieId);
-            console.log(film);
-            films.push(film);
-            this.createMarkUp(this.preparingForMarkUp(films));
+            const movie = await Api.fetchMovieDetail(movieId);
+            console.log(movie);
+            movies.push(movie);
+            this.createMarkUp(this.preparingForMarkUp(movies));
         } catch (error) {
             console.log(error, `Попробуйте перезагрузить страницу`);
         }
-        });
-    console.log(films);
-    return(films);
+    });
+    return(movie);
     },
 
     queueRender() {
     const queueMoviesID = JSON.parse(localStorage.getItem("storage")).que;
-    console.log(queueMoviesID);
-    const films = [];
-        queueMoviesID.map(async (movieId) => {
-        this.options.id = movieId;
-        console.log(this.options.id);
+    const movies = [];
+        
+    queueMoviesID.map(async (movieId) => {
         try {
-            const film = await Api.fetchMovieDetail(movieId);
-            console.log(film);
-            films.push(film);
-            this.createMarkUp(this.preparingForMarkUp(films));
+            const movie = await Api.fetchMovieDetail(movieId);
+            console.log(movie);
+            movies.push(movie);
+            this.createMarkUp(this.preparingForMarkUp(movies));
         } catch (error) {
             console.log(error, `Попробуйте перезагрузить страницу`);
         }
-        });
-    console.log(films);
-    return(films);
+    });
+    return(movies);
     },
 
-preparingForMarkUp(films) {
-    return films.map(
+preparingForMarkUp(movies) {
+    return movies.map(
         ({ id, title, poster_path, vote_average, release_date, genres }) => ({
         id,
         title,
@@ -59,10 +53,10 @@ preparingForMarkUp(films) {
         genres_id: genres[0].name,
         // genres_id: this.calculatingGenres(genres),
         release_date: release_date.split("-"),
-        
         })
     );
     },
+    
     createMarkUp(preparedMovies) {
     const { movieList } = this;
     const moviesMarkUp = preparedMovies
@@ -81,39 +75,34 @@ preparingForMarkUp(films) {
             <div class="gallery__stats">
                 <p class="gallery__details">${genres_id} | ${
             release_date[0]}</p>
-
-                <p p class="gallery__rating">${vote_average.toFixed(1)}</p>
+            <p p class="gallery__rating">${vote_average.toFixed(1)}</p>
             </div>
             </div>
-        </li>
-    `;
-        }
-        )
-        .join("");
-    movieList.innerHTML = "";
-    movieList.insertAdjacentHTML("beforeend", moviesMarkUp);
+        </li>`;
+            }).join("");
+        
+        movieList.innerHTML = "";
+        movieList.insertAdjacentHTML("beforeend", moviesMarkUp);
+        modalListener();
     },
 
-    
-
     calculatingGenres(genres) {
-      console.log(genres);
-    const sortGenres = allGenres
-      .filter((genre) => {
+    console.log(genres);
+    const sortGenres = allGenres.filter((genre) => {
         for (const id of genres) {
-          if (id === genre.id) {
+            if (id === genre.id) {
             return genre;
-          }
+            }
         }
-      })
-        .map((genre) => genre.name);
-        console.log(sortGenres);
+    }).map((genre) => genre.name);
+    console.log(sortGenres);
+    
     if (sortGenres.length > 2) {
-      return [...sortGenres.slice(0, 2), "Other"];
+        return [...sortGenres.slice(0, 2), "Other"];
     } else {
-      return sortGenres;
+        return sortGenres;
     }
-  },
+},
 }
 
 // <p class="gallery__details">${genres_id.join(", ")} | ${release_date[0]}</p>
